a4af3fcd2225598fc1fb327be019e9b484562419
Peter Morgenstern <mail@petergundel.de>
2025-09-15 21:33:47 +0200

    yazi: Add plugin starship

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -36,51 +36,54 @@ require("bunny"):setup({
 	hops = bookmarks,
 })
 
-require("yatline"):setup({
-	show_background = false,
-
-	header_line = {
-		left = {
-			section_a = {
-				{ type = "coloreds", custom = false, name = "githead" },
-			},
-			section_b = {},
-			section_c = {},
-		},
-		right = {
-			section_a = {
-				{ type = "line", custom = false, name = "tabs", params = { "right" } },
-			},
-			section_b = {},
-			section_c = {},
-		},
-	},
-
-	status_line = {
-		left = {
-			section_a = {
-				{ type = "string", custom = false, name = "tab_mode" },
-			},
-			section_b = {
-				{ type = "string", custom = false, name = "hovered_size" },
-			},
-			section_c = {
-				{ type = "string", custom = false, name = "hovered_path" },
-				{ type = "coloreds", custom = false, name = "count" },
-			},
-		},
-		right = {
-			section_a = {
-				{ type = "string", custom = false, name = "cursor_position" },
-			},
-			section_b = {
-				{ type = "string", custom = false, name = "cursor_percentage" },
-			},
-			section_c = {
-				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
-				{ type = "coloreds", custom = false, name = "permissions" },
-			},
-		},
-	},
+-- require("yatline"):setup({
+-- 	show_background = false,
+--
+-- 	header_line = {
+-- 		left = {
+-- 			section_a = {
+-- 				{ type = "coloreds", custom = false, name = "githead" },
+-- 			},
+-- 			section_b = {},
+-- 			section_c = {},
+-- 		},
+-- 		right = {
+-- 			section_a = {
+-- 				{ type = "line", custom = false, name = "tabs", params = { "right" } },
+-- 			},
+-- 			section_b = {},
+-- 			section_c = {},
+-- 		},
+-- 	},
+--
+-- 	status_line = {
+-- 		left = {
+-- 			section_a = {
+-- 				{ type = "string", custom = false, name = "tab_mode" },
+-- 			},
+-- 			section_b = {
+-- 				{ type = "string", custom = false, name = "hovered_size" },
+-- 			},
+-- 			section_c = {
+-- 				{ type = "string", custom = false, name = "hovered_path" },
+-- 				{ type = "coloreds", custom = false, name = "count" },
+-- 			},
+-- 		},
+-- 		right = {
+-- 			section_a = {
+-- 				{ type = "string", custom = false, name = "cursor_position" },
+-- 			},
+-- 			section_b = {
+-- 				{ type = "string", custom = false, name = "cursor_percentage" },
+-- 			},
+-- 			section_c = {
+-- 				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+-- 				{ type = "coloreds", custom = false, name = "permissions" },
+-- 			},
+-- 		},
+-- 	},
+-- })
+-- require("yatline-githead"):setup()
+require("starship"):setup({
+  config_file = "~/.config/yazi/starship.toml",
 })
-require("yatline-githead"):setup()
