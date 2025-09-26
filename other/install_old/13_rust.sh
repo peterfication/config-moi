d3cee2963b23edf9979f263199ec21dc68aff22a
Peter Morgenstern <mail@petergundel.de>
2025-09-26 20:43:58 +0200

    Add old install scripts

---
+++ other/install_old/13_rust.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# lorikeet
+# click
+
+cargo install zellij
+cargo install --git https://github.com/MordechaiHadad/bob.git
+cargo install csvlens
+
+# A curated list of command-line utilities written in Rust: https://gist.github.com/sts10/daadbc2f403bdffad1b6d33aff016c0a
