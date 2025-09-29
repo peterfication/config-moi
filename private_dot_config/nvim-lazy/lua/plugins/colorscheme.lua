e25a4797f8cc171d80843d3bce3fe65c1d553b80
Peter Morgenstern <mail@petergundel.de>
2025-09-29 06:30:18 +0200

    nvim-lazy: Change colorscheme to solarized

---
+++ private_dot_config/nvim-lazy/lua/plugins/colorscheme.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {
+
+    },
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+
+      require("solarized").setup(opts)
+
+      vim.cmd.colorscheme("solarized")
+
+      -- Commands to change background between dark to light
+      vim.api.nvim_create_user_command("ColorschemeDark", function()
+        vim.o.background = "dark"
+      end, {})
+      vim.api.nvim_create_user_command("ColorschemeLight", function()
+        vim.o.background = "light"
+      end, {})
+      vim.api.nvim_create_user_command("ColorschemeToggle", function()
+        if vim.o.background == "dark" then
+          vim.o.background = "light"
+        else
+          vim.o.background = "dark"
+        end
+      end, {})
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized",
+    },
+  },
+}
