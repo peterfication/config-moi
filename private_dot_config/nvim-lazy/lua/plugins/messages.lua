2ca8d29e0d4f5f0f20e33b13c6b404e5a430149a
Peter Morgenstern <mail@petergundel.de>
2025-09-29 08:58:11 +0200

    nvim-lazy: Setup nvim-notify

---
+++ private_dot_config/nvim-lazy/lua/plugins/messages.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "20%",
+          },
+        },
+        -- notify = {
+        --   win_options = {
+        --     winblend = 80
+        --   },
+        -- },
+      },
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    opts = {
+      top_down = false,
+      on_open = function(win)
+        -- vim.api.nvim_win_set_option(win, "winblend", 80)
+      end,
+    },
+    config = function()
+      -- require("lualine").setup({
+      --   sections = {
+      --     lualine_x = {
+      --       "encoding",
+      --       "fileformat",
+      --       "filetype",
+      --       {
+      --         require("noice").api.statusline.mode.get,
+      --         cond = require("noice").api.statusline.mode.has,
+      --         color = { fg = "#ff9e64" },
+      --       },
+      --     },
+      --   },
+      -- })
+    end,
+    keys = {
+      { "<Leader>M",  group = "Noice" },
+      { "<Leader>MC", ":lua require('notify').dismiss()<CR>", desc = "Close all Noice messages" },
+      { "<Leader>MF", ":Noice telescope<CR>",                 desc = "Open Noice messages in Telescope" },
+      { "<Leader>MM", ":Noice<CR>",                           desc = "Open Noice messages" },
+      { "<Leader>Mf", ":Noice telescope<CR>",                 desc = "Open Noice messages in Telescope" },
+    },
+  },
+}
--- private_dot_config/nvim-lazy/lua/plugins/snacks.lua
+++ private_dot_config/nvim-lazy/lua/plugins/snacks.lua
@@ -3,6 +3,7 @@ return {
     "snacks.nvim",
     opts = {
       scroll = { enabled = false },
+      notifier = { enabled = false },
     },
   },
 }
