0c6b90d34a073e5c83e0ed8d8bd1ab034b68a70c
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-09-29 21:36:12 +0200

    nvim-lauy: Add treewalker

---
+++ private_dot_config/nvim-lazy/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "aaronik/treewalker.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    keys = {
+      { "<A-k>", "<CMD>Treewalker Up<CR>", silent = true, mode = { "n", "v" } },
+      { "<A-j>", "<CMD>Treewalker Down<CR>", silent = true, mode = { "n", "v" } },
+      { "<A-h>", "<CMD>Treewalker Left<CR>", silent = true, mode = { "n", "v" } },
+      { "<A-l>", "<CMD>Treewalker Right<CR>", silent = true, mode = { "n", "v" } },
+
+      { "<A-S-k>", "<CMD>Treewalker SwapUp<CR>", silent = true },
+      { "<A-S-j>", "<CMD>Treewalker SwapDown<CR>", silent = true },
+      { "<A-S-h>", "<CMD>Treewalker SwapLeft<CR>", silent = true },
+      { "<A-S-l>", "<CMD>Treewalker SwapRight<CR>", silent = true },
+    },
+  },
+}
