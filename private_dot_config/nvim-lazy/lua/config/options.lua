6f2bc1819c8b9062de29ad1ccc339db1762c20e7
Peter Morgenstern <mail@petergundel.de>
2025-09-29 09:36:22 +0200

    nvim-lazy: Set vim.opt.listchars

--- private_dot_config/nvim-lazy/lua/config/options.lua
+++ private_dot_config/nvim-lazy/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.g.lazyvim_picker = "telescope"
+
+vim.opt.listchars = {
+  trail = "¤",
+  nbsp = "·",
+  extends = ">",
+  precedes = "<",
+  space = "·",
+  eol = "↵",
+  -- tab = '»', -- tab does not work
+}
