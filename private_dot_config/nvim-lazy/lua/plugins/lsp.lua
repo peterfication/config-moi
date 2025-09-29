bcd75182175a70f8fd8542bddb266fd88565c32f
Peter Morgenstern <mail@petergundel.de>
2025-09-29 11:40:30 +0200

    nvim-lazy: Adjust LSP keymaps <Leader>c to <Leader>z

--- private_dot_config/nvim-lazy/lua/plugins/lsp.lua
+++ private_dot_config/nvim-lazy/lua/plugins/lsp.lua
@@ -10,10 +10,46 @@ return {
     --   -- add a keymap
     --   keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
     -- end,
+    opts = function()
+      -- See
+      -- https://www.lazyvim.org/plugins/lsp
+      -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/lsp/keymaps.lua
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      -- Change keympas von <Leader>c to <Leader>z
+      keys[#keys + 1] = { "<Leader>ca", false }
+      keys[#keys + 1] = { "<Leader>cc", false }
+      keys[#keys + 1] = { "<Leader>cC", false }
+      keys[#keys + 1] = { "<Leader>cR", false }
+      keys[#keys + 1] = { "<Leader>cr", false }
+      keys[#keys + 1] = { "<Leader>cA", false }
+
+      keys[#keys + 1] =
+      { "<leader>za", vim.lsp.buf.code_action, desc = "[LSP] Code Action", mode = { "n", "v" }, has = "codeAction" }
+
+      keys[#keys + 1] =
+      { "<leader>zc", vim.lsp.codelens.run, desc = "[LSP] Run Codelens", mode = { "n", "v" }, has = "codeLens" }
+      keys[#keys + 1] = {
+        "<leader>zC",
+        vim.lsp.codelens.refresh,
+        desc = "[LSP] Refresh & Display Codelens",
+        mode = { "n" },
+        has = "codeLens",
+      }
+      keys[#keys + 1] = {
+        "<leader>zR",
+        function()
+          Snacks.rename.rename_file()
+        end,
+        desc = "[LSP] Rename File",
+        mode = { "n" },
+        has = { "workspace/didRenameFiles", "workspace/willRenameFiles" },
+      }
+      keys[#keys + 1] = { "<leader>zr", vim.lsp.buf.rename, desc = "[LSP] Rename", has = "rename" }
+      keys[#keys + 1] = { "<leader>zA", LazyVim.lsp.action.source, desc = "[LSP] Source Action", has = "codeAction" }
+    end,
     keys = {
-      { "<Leader>z",  group = "LSP" },
-      { "<Leader>za", "<CMD>lua vim.lsp.buf.code_action()<CR>", desc = "[LSP] Code action" },
-      { "<Leader>zr", "<CMD>lua vim.lsp.buf.rename()<CR>",      desc = "[LSP] Rename" },
+      { "<Leader>z", group = "LSP" },
     },
   },
 
