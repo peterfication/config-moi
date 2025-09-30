07564b770e632b35d165ed24ec0d16c777310457
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-09-30 07:59:01 +0200

    nvim-lazy: More keymaps for textobjects

--- private_dot_config/nvim-lazy/lua/plugins/treesitter.lua
+++ private_dot_config/nvim-lazy/lua/plugins/treesitter.lua
@@ -1,5 +1,62 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    opts = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["aj"] = "@comment.outer",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+          ["ah"] = "@call.outer",
+          ["ih"] = "@call.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["ak"] = "@conditional.outer",
+          ["ik"] = "@conditional.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["glfn"] = "@function.outer",
+          ["glcn"] = "@class.outer",
+          ["glkn"] = "@conditional.outer",
+          ["glln"] = "@loop.outer",
+          ["glbn"] = "@block.outer",
+          ["glhn"] = "@call.outer",
+          ["gljn"] = "@comment.outer",
+        },
+        goto_next_end = {
+          ["glfe"] = "@function.outer",
+          ["glce"] = "@class.outer",
+          ["glke"] = "@conditional.outer",
+          ["glle"] = "@loop.outer",
+          ["glbe"] = "@block.outer",
+          ["glhe"] = "@call.outer",
+          ["glje"] = "@comment.outer",
+        },
+        goto_previous_start = {
+          ["glfp"] = "@function.outer",
+          ["glcp"] = "@class.outer",
+          ["glkp"] = "@conditional.outer",
+          ["gllp"] = "@loop.outer",
+          ["glbp"] = "@block.outer",
+          ["glhp"] = "@call.outer",
+          ["gljp"] = "@comment.outer",
+        },
+        goto_previous_end = {},
+      },
+    },
+  },
+
+  {
     "aaronik/treewalker.nvim",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
