ce7239f9679ece382f547bbdf4da20e802fc6d2b
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-10-06 07:34:31 +0200

    nvim-lazy: cmp: Disable ghost text

---
+++ private_dot_config/nvim-lazy/lua/plugins/auto-completion.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    {
+      "saghen/blink.cmp",
+      opts = {
+        completion = {
+          menu = {
+            auto_show = true,
+          },
+          ghost_text = {
+            show_with_menu = false,
+          },
+        },
+      },
+    },
+  },
+}
