5d2e0fa197b441ce34a9e8cc53d4c390122ba2a3
Peter Morgenstern <mail@petergundel.de>
2025-10-06 21:57:35 +0200

    zsh_new: Add theme via tinty

---
+++ private_dot_config/zsh_new/05_theme.zsh
@@ -0,0 +1,11 @@
+# Load tinty theme
+#
+# See
+# - https://github.com/tinted-theming/tinted-shell
+# - https://github.com/tinted-theming/tinty/blob/main/USAGE.md#shell
+#
+# NOTE: Don't load iTerm2 oascript because it adds a noticable delay.
+tinty apply base16-solarized-dark
+
+alias tinty-light='tinty apply base16-solarized-light'
+alias tinty-dark='tinty apply base16-solarized-dark'
