8b6445b64073e12862f680e6043422439ffcc4fe
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-10-06 08:57:04 +0200

    nvim-lazy: Use Esc to cancel auto completion

--- private_dot_config/nvim-lazy/lua/plugins/auto-completion.lua
+++ private_dot_config/nvim-lazy/lua/plugins/auto-completion.lua
@@ -11,6 +11,22 @@ return {
             show_with_menu = false,
           },
         },
+        keymap = {
+          ["<Esc>"] = { "hide", "fallback" },
+        },
+        cmdline = {
+          keymap = {
+            ["<ESC>"] = {
+              function(cmp)
+                if cmp.is_visible() then
+                  cmp.cancel()
+                else
+                  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", true)
+                end
+              end,
+            },
+          },
+        },
       },
     },
   },
