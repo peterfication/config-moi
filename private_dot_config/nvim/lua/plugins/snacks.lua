14900df4391ac44957c4e57fab38de032bf904a2
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-12-02 11:16:03 +0100

    nvim: Snacks.lazygit: Don't open in new tab

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -9,6 +9,18 @@ return {
         },
       },
       notifier = { enabled = false },
+      lazygit = {
+        config = {
+          os = {
+            -- These values differ from the preset in that they don't open a new tab
+            -- https://github.com/jesseduffield/lazygit/blob/d1d2bb23b6b9d862fbd16aac20b029eec01ddc8d/pkg/config/editor_presets.go#L64
+            edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+            editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+            editAtLineAndWait = 'nvim +{{line}} {{filename}}',
+            openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
+          },
+        },
+      }
     },
   },
 
