a415dd8c49c5eaea68e265ff1c3d830ba83fc5fa
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-02-12 22:32:44 +0100

    nix: Move iterm2 from brew to nix

--- private_dot_config/nix/flake.nix
+++ private_dot_config/nix/flake.nix
@@ -24,6 +24,7 @@
         eza # https://github.com/eza-community/eza
         fd # https://github.com/sharkdp/fd
         fzf # https://github.com/junegunn/fzf
+        iterm2 # https://github.com/gnachman/iTerm2
         jjui # https://github.com/idursun/jjui
         jujutsu # https://jj-vcs.github.io/jj/latest/
         just # https://github.com/casey/just
