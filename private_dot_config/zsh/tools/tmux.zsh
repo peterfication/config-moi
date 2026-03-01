d67d3cfca1063ebda1dd3cb6b9849ca27bcf5f35
Peter Morgenstern <mail@petergundel.de>
2026-03-01 10:52:17 +0100

    tmux: Fix TERM part 2

--- private_dot_config/zsh/tools/tmux.zsh
+++ private_dot_config/zsh/tools/tmux.zsh
@@ -1,4 +1,4 @@
-alias tm='env TERM=screen-256color tmux -2'
+alias tm='tmux'
 
 export TMUX_SESSIONS_FILE="$HOME/.config/tmux/sessions.txt"
 
@@ -29,22 +29,13 @@ function tmux_new_with_name() {
     echo "Create session '${DIR_NAME}' with window 'nvim' and folder '${TS_DIR}'"
     eval "tmux new-session -d -c ${TS_DIR} -s ${DIR_NAME} -n nvim"
     eval "tmux send-keys -t 1.0 nvim Enter"
-
-    # echo "Split window 'console' in session '${DIR_NAME}'"
-    # eval "tmux split-window -h -t ${DIR_NAME} -c ${TS_DIR}"
-
-    # echo "Create window 'console' in session '${DIR_NAME}' with folder '${TS_DIR}'"
-    # eval "tm new-window -t ${DIR_NAME}: -c ${TS_DIR} -n console"
-
-    # Show nvim window
-    # eval "tm next-window"
   done
 }
 
 function ts() {
   tmux_new_with_name `pwd`
   DIR_NAME=`pwd | grep -o "[a-zA-Z0-9._-]*$" | sed -r 's/[.]+/-/g'`
-  tm a -t $DIR_NAME
+  tmux a -t $DIR_NAME
 }
 
 # This function is for tmux session creations. See tmux/04_mappings.tmux
