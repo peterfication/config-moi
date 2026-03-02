e750e88ac2abaf4f932464b926655653a2ccf9cf
Peter Morgenstern <mail@petergundel.de>
2026-03-02 10:55:23 +0100

    Add mise to install-all

--- private_dot_config/nix/shellAliases.nix
+++ private_dot_config/nix/shellAliases.nix
@@ -3,7 +3,7 @@
   nix-rebuild = "(cd ~/.config/nix && just nix-darwin-rebuild)";
   # Run bundle install from the brew config directory
   brew-install = "(cd ~/.config/brew && just brew-install)";
-  install-all = "nix-rebuild && brew-install";
+  install-all = "nix-rebuild && brew-install && mise install";
   # Open vim with the chezmoi config
   conf = " cd $(chezmoi source-path) && nvim";
 }
