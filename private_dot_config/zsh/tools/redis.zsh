2b7f62ca560280c12f9e70aba1debb0ea7cf253c
Peter Morgenstern <mail@petergundel.de>
2026-03-05 10:16:41 +0100

    zsh: Prefix service aliases with s-
    ... for better auto completion.

--- private_dot_config/zsh/tools/redis.zsh
+++ private_dot_config/zsh/tools/redis.zsh
@@ -5,7 +5,7 @@ export REDIS_RUNDIR="$REDIS_HOME/run"
 export REDIS_LOGFILE="$REDIS_LOGDIR/redis.log"
 export REDIS_PIDFILE="$REDIS_RUNDIR/redis.pid"
 
-alias redis_service_start='mkdir -p "$REDIS_DATA" "$REDIS_LOGDIR" "$REDIS_RUNDIR" && redis-server --daemonize yes --bind 127.0.0.1 --port 6379 --dir "$REDIS_DATA" --pidfile "$REDIS_PIDFILE" --logfile "$REDIS_LOGFILE"'
-alias redis_service_stop='redis-cli -p 6379 shutdown'
-alias redis_service_status='redis-cli -p 6379 ping'
-alias redis_service_logs_tail='tail -n 200 -f "$REDIS_LOGFILE"'
+alias s-redis_service_start='mkdir -p "$REDIS_DATA" "$REDIS_LOGDIR" "$REDIS_RUNDIR" && redis-server --daemonize yes --bind 127.0.0.1 --port 6379 --dir "$REDIS_DATA" --pidfile "$REDIS_PIDFILE" --logfile "$REDIS_LOGFILE"'
+alias s-redis_service_stop='redis-cli -p 6379 shutdown'
+alias s-redis_service_status='redis-cli -p 6379 ping'
+alias s-redis_service_logs_tail='tail -n 200 -f "$REDIS_LOGFILE"'
