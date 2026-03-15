f93d1d35eee7195aceb1bde734997fc50b865a0d
Peter Morgenstern <mail@petergundel.de>
2026-03-15 21:17:37 +0100

    hammerspoon: Add experimental hyperSpace key layer

--- private_dot_config/hammerspoon/init.lua
+++ private_dot_config/hammerspoon/init.lua
@@ -16,3 +16,24 @@ hs.hotkey.bind({ "alt", "ctrl", "cmd", "shift" }, "ö", function()
 
   spoon.Caffeine:clicked()
 end)
+
+hyperSpace = hs.hotkey.modal.new({ "ctrl", "alt", "cmd", "shift" }, "space")
+
+function hyperSpace:entered()
+  hs.alert.show("Hyper+Space mode")
+  hs.timer.doAfter(2, function()
+    hs.alert.show("Hyper+Space mode timeout")
+    hyperSpace:exit()
+  end)
+end
+
+function hyperSpace:exited()
+  hs.timer.doAfter(0.5, function()
+    hs.alert.closeAll()
+  end)
+end
+
+hyperSpace:bind({}, "f", function()
+  hs.alert.show("Hyper+Space+F pressed!")
+  hyperSpace:exit()
+end)
