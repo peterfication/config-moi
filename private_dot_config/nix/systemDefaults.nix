f835b00545d8d2aba82c917af6238b63d9e5d5e3
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-03-30 07:50:48 +0200

    nix: Add Finder key binding for Ghostty Window

--- private_dot_config/nix/systemDefaults.nix
+++ private_dot_config/nix/systemDefaults.nix
@@ -7,4 +7,14 @@
     KeyRepeat = 3; # Lower is faster
     InitialKeyRepeat = 15; # Lower is sooner
   };
+
+  CustomUserPreferences = {
+    pbs = {
+      NSServicesStatus = {
+        "com.mitchellh.ghostty - New Ghostty Window Here - openWindow" = {
+          key_equivalent = "@~^$t";
+        };
+      };
+    };
+  };
 }
