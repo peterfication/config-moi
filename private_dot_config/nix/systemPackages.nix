4a3c598f85136553b6b594f58dbdda8b0b24a0a8
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-03-31 19:18:29 +0200

    nix: Add arp-scan

--- private_dot_config/nix/systemPackages.nix
+++ private_dot_config/nix/systemPackages.nix
@@ -11,6 +11,7 @@ with pkgs; [
   redis
 
   age # https://github.com/FiloSottile/age
+  arp-scan # https://github.com/royhills/arp-scan
   bitwarden-cli # https://github.com/bitwarden/clients
   chezmoi # https://www.chezmoi.io
   delta # https://github.com/dandavison/delta
