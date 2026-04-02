68dbdcb2a152e650caf1b99ccf5237ff06ecf2f0
Peter Morgenstern <mail@petergundel.de>
2026-04-02 20:26:59 +0200

    nvim: Use default gitsigns setup

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -56,84 +56,6 @@ return {
       })
     end,
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-    keys = {
-      {
-        "<Leader>GG",
-        desc = "DiffView",
-      },
-      {
-        "<Leader>GGF",
-        ":DiffviewFileHistory %<CR>",
-        desc = "Open DiffView with the Git history of the current file",
-      },
-      {
-        "<Leader>GGM",
-        ":DiffviewOpen<CR>",
-        desc = "Open DiffView (e.g. for merge conflicts)",
-      },
-      {
-        "<Leader>GGR",
-        ":DiffviewOpen origin/main...HEAD<CR>",
-        desc = "Open DiffView for current branch against main (like a PR)",
-      },
-    },
-  },
-
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      numhl = true,
-      current_line_blame_opts = {
-        delay = 100,
-      },
-
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map("n", "ghn", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
-
-        map("n", "ghp", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
-
-        -- Actions
-        -- map({'n', 'v'}, '<Leader>hs', ':Gitsigns stage_hunk<CR>')
-        -- map({'n', 'v'}, '<Leader>hr', ':Gitsigns reset_hunk<CR>')
-        -- map('n', '<Leader>hS', gs.stage_buffer)
-        -- map('n', '<Leader>hu', gs.undo_stage_hunk)
-        -- map('n', '<Leader>hR', gs.reset_buffer)
-        map("n", "<Leader>ghs", gs.preview_hunk)
-        map("n", "<Leader>ghr", gs.reset_hunk)
-        -- map('n', '<Leader>hb', function() gs.blame_line{full=true} end)
-        map("n", "<Leader>gb", gs.toggle_current_line_blame)
-        map("n", "<Leader>ghd", gs.diffthis)
-        map("n", "<Leader>ghD", function()
-          gs.diffthis("~")
-        end)
-        -- map('n', '<Leader>td', gs.toggle_deleted)
-      end,
-    },
     keys = function()
       local tig_blame_term_open = function()
         local file = vim.fn.expand("%")
@@ -150,7 +72,25 @@ return {
       end
 
       return {
-        { "<Leader>gb", ":Gitsigns toggle_current_line_blame<CR>", desc = "Toogle Git blame current line" },
+        {
+          "<Leader>GG",
+          desc = "DiffView",
+        },
+        {
+          "<Leader>GGF",
+          ":DiffviewFileHistory %<CR>",
+          desc = "Open DiffView with the Git history of the current file",
+        },
+        {
+          "<Leader>GGM",
+          ":DiffviewOpen<CR>",
+          desc = "Open DiffView (e.g. for merge conflicts)",
+        },
+        {
+          "<Leader>GGR",
+          ":DiffviewOpen origin/main...HEAD<CR>",
+          desc = "Open DiffView for current branch against main (like a PR)",
+        },
         { "<Leader>GB", tig_blame_term_open, desc = "Open tig for current file" },
       }
     end,
