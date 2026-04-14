b147aff81ad7771ff9520983b939c6fca408b37a
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-04-14 11:36:26 +0200

    nvim: Snacks LSP restart key binding

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -55,4 +55,42 @@ return {
       },
     },
   },
+
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts.picker = opts.picker or {}
+      opts.picker.actions = opts.picker.actions or {}
+      opts.picker.sources = opts.picker.sources or {}
+
+      opts.picker.actions.lsp_restart = function(_, item)
+        if not item or not item.name then
+          return
+        end
+        vim.cmd("lsp restart " .. item.name)
+        Snacks.notifier.notify("LSP " .. item.name .. " restarted", "info", {
+          style = "minimal",
+        })
+      end
+
+      opts.picker.sources.lsp_config = vim.tbl_deep_extend("force", opts.picker.sources.lsp_config or {}, {
+        win = {
+          input = {
+            keys = {
+              ["<C-r>"] = { "lsp_restart", mode = { "n", "i" }, desc = "Restart LSP" },
+            },
+          },
+        },
+      })
+    end,
+    keys = {
+      {
+        "<Leader>cL",
+        function()
+          Snacks.picker.lsp_config({ attached = 0 })
+        end,
+        desc = "LSP Info (attached only)",
+      },
+    },
+  },
 }
