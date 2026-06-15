a43cc0295312eb1a860313d019eaf11d8e6aa2f8
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-06-15 08:48:37 +0200

    nix: Disable "Hide Others" keymap

--- private_dot_config/nix/systemDefaults.nix
+++ private_dot_config/nix/systemDefaults.nix
@@ -9,6 +9,14 @@
   };
 
   CustomUserPreferences = {
+    NSGlobalDomain = {
+      NSUserKeyEquivalents = {
+        # Control+Option+Shift+F20, intentionally unavailable on the physical keyboard.
+        "Hide Others" = builtins.fromJSON ''"^~$\uf717"'';
+        "Andere ausblenden" = builtins.fromJSON ''"^~$\uf717"'';
+      };
+    };
+
     pbs = {
       NSServicesStatus = {
         "com.mitchellh.ghostty - New Ghostty Window Here - openWindow" = {
