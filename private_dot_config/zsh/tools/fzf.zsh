4d2a6c5695811770093b7aa63e6294160e3b1d43
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-07-05 19:28:23 +0200

    zsh: Improve kill-process ergonomics
    - Uses its first argument as the initial fzf --query.
    - Captures the last fzf input via --print-query.
    - Reopens itself after Enter with that query prefilled.
    - Keeps an optional second argument for the kill signal, defaulting to 9.

--- private_dot_config/zsh/tools/fzf.zsh
+++ private_dot_config/zsh/tools/fzf.zsh
@@ -3,12 +3,18 @@ export FZF_DEFAULT_COMMAND="rg --files"
 # From https://medium.com/@sidneyliebrand/how-fzf-and-ripgrep-improved-my-workflow-61c7ca212861
 # mnemonic: [K]ill [P]rocess
 kill-process() {
-  local pid=$(ps -ef | sed 1d | eval "fzf  -m --header='[kill:process]'" | awk '{print $2}')
+  local query="${1:-}"
+  local signal="${2:-9}"
+  local -a fzf_output selections
+  fzf_output=("${(@f)$(ps -ef | sed 1d | fzf -m --print-query --query="$query" --header='[kill:process]')}")
+  query="${fzf_output[1]}"
+  selections=("${fzf_output[@]:1}")
+  local pid=$(printf '%s\n' "${selections[@]}" | awk '{print $2}')
 
   if [ "x$pid" != "x" ]
   then
-    echo $pid | xargs sudo kill -${1:-9}
-    kill-process
+    echo $pid | xargs sudo kill -"$signal"
+    kill-process "$query" "$signal"
   fi
 }
 alias kp=kill-process
