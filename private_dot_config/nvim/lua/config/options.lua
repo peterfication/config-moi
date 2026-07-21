4893499955b9f570274d4abc153ecc5aff29e8fe
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-07-21 13:18:10 +0200

    nvim: Set ty as Python LSP

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.g.lazyvim_picker = "telescope"
+vim.g.lazyvim_python_lsp = "ty"
 
 vim.g.maplocalleader = "  "
 
