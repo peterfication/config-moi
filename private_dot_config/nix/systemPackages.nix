9a301e1b8adfeb8067c47afaa4e46122a855a713
Peter Morgenstern <peter.morgenstern@prenode.de>
2026-08-11 15:34:52 +0200

    nix: Add luarocks

--- private_dot_config/nix/systemPackages.nix
+++ private_dot_config/nix/systemPackages.nix
@@ -40,6 +40,7 @@ with pkgs; [
   pkgsUnstable.lazydocker # https://github.com/jesseduffield/lazydocker
   pkgsUnstable.lefthook # https://github.com/evilmartians/lefthook
   lnav # https://github.com/tstack/lnav
+  luarocks # https://github.com/luarocks/luarocks
   pkgsUnstable.mise # https://github.com/jdx/mise
   ncftp # https://www.ncftp.com/ncftp/
   natscli # https://nats.io/
